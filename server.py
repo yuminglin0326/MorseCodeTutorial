20944d9c68e52a2d64174e59f44748037794924b
Yuming Lin <yuminglin0326@gmail.com>
2024-04-15 14:15:27 -0400

    create empty quiz1 page

--- server.py
+++ server.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/quiz1')
+def quiz1():
+    return render_template('quiz1.html')
+
 
 if __name__ == '__main__':
     app.run(debug = True)
