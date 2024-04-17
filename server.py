c3c4b393a68b96f975775aeb76a6ba1f9192712b
Yuming Lin <yuminglin0326@gmail.com>
2024-04-16 20:37:10 -0400

    Add data model for quiz page

--- server.py
+++ server.py
@@ -3,14 +3,25 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
+quizzes = {
+    "1": {
+        "id": 1,
+        "name": "Quiz 1",
+        "question": "Please enter the Morse Code for 'I'",
+        "answer": "..",
+    }
+}
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/quiz1')
-def quiz1():
-    return render_template('quiz1.html')
+@app.route('/quiz/<quiz_id>')
+def quiz(quiz_id):
+    quiz = quizzes[quiz_id]
+
+    return render_template('quiz.html', quiz=quiz)
 
 
 if __name__ == '__main__':
